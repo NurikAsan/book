78b2d4cab5be270f4abb9f5bd6db75fbd34dc113
Nurikov <asankylovnursultan@gmail.com>
2024-08-26 00:03:35 +0600

    add filter search and ordering

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    author_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
--- apps/store/views.py
+++ apps/store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ('name', )
+    search_fields = ('name', 'author_name')
+    ordering_fields = ('price', 'author_name')
